64d8e9e70b8effe0785ebfabb607cb1329967854
John Kim <mkcokr2176@gmail.com>
2024-04-28 20:49:56 -0700

    Remove comments

--- autotrade.py
+++ autotrade.py
@@ -14,20 +14,12 @@ from io import BytesIO
 
 load_dotenv()
 
-
 # client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 #coinBaseclient = Client( os.getenv('API_KEY'), os.getenv('API_SECRET'))
 coinBaseclient = RESTClient( os.getenv('API_KEY2'), os.getenv('API_SECRET2'))
 # coinBaseclient = RESTClient(api_key=api_key, api_secret=api_secret)
 
-
 s3 = boto3.client('s3')
-# s3.upload_file('some-text', 'cryptocurrencys3', 'some-text')
-
-# Upload a text string to S3
-# text_content = "This is a test text file."
-# file_obj = BytesIO(text_content.encode())
-# s3.upload_fileobj(file_obj, 'cryptocurrencys3', 'information/test-text.txt')
 
 def get_coinbase_market_data():
     url = "https://api.pro.coinbase.com/products/BTC-USD/candles"
@@ -213,7 +205,6 @@ if __name__ == "__main__":
     openaiTesting()
     # schedule.every().hour.at(":01").do(openaiTesting)
     schedule.every(5).seconds.do(openaiTesting)
-
 
     while True:
         schedule.run_pending()
